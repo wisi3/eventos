061eb17b2b15ee9a94a9535f05be914a7c2f5bd7
Angel Sullon <asullom@gmail.com>
2017-10-27 10:48:26 -0500

    form

--- src/components/Layout01.js
+++ src/components/Layout01.js
@@ -4,52 +4,8 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+import {routes} from '../routes'
 
-
-////
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-const Sandwiches = () => <h2>Sandwiches</h2>
-
-const Tacos = ({ routes }) => (
-  <div>
-    <h2>Tacos</h2>
-    <ul>
-      <li><Link to="/tacos/bus">Bus</Link></li>
-      <li><Link to="/tacos/cart">Cart</Link></li>
-    </ul>
-
-    {routes.map((route, i) => (
-      <RouteWithSubRoutes key={i} {...route}/>
-    ))}
-  </div>
-)
-
-const Bus = () => <h3>Bus</h3>
-const Cart = () => <h3>Cart</h3>
-
-const routes = [
-  { path: '/home',
-    component: Home
-  },
-{ path: '/sandwiches',
-    component: Sandwiches
-  },
-  { path: '/tacos',
-    component: Tacos,
-    routes: [
-      { path: '/tacos/bus',
-        component: Bus
-      },
-      { path: '/tacos/cart',
-        component: Cart
-      }
-    ]
-  }
-]
 
 const RouteWithSubRoutes = (route) => (
   <Route path={route.path} render={props => (
@@ -71,8 +27,7 @@ const SidebarExample = () => (
            <li><Link to="/tacos">Tacos</Link></li>
         <li><Link to="/sandwiches">Sandwiches</Link></li>
         </ul>
-
-        
+       
       </div>
 
       <div style={{ flex: 1, padding: '10px' }}>
---
+++ src/routes.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import {
+     Route,
+  Link
+} from 'react-router-dom'
+
+const RouteWithSubRoutes = (route) => (
+  <Route path={route.path} render={props => (
+    // pass the sub-routes down to keep nesting
+    <route.component {...props} routes={route.routes}/>
+  )}/>
+)
+
+////
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+const Sandwiches = () => <h2>Sandwiches</h2>
+
+const Tacos = ({ routes }) => (
+  <div>
+    <h2>Tacos</h2>
+    <ul>
+      <li><Link to="/tacos/bus">Bus</Link></li>
+      <li><Link to="/tacos/cart">Cart</Link></li>
+    </ul>
+
+    {routes.map((route, i) => (
+      <RouteWithSubRoutes key={i} {...route}/>
+    ))}
+  </div>
+)
+
+const Bus = () => <h3>Bus</h3>
+const Cart = () => <h3>Cart</h3>
+
+const routes = [
+  { path: '/home',
+    component: Home
+  },
+{ path: '/sandwiches',
+    component: Sandwiches
+  },
+  { path: '/tacos',
+    component: Tacos,
+    routes: [
+      { path: '/tacos/bus',
+        component: Bus
+      },
+      { path: '/tacos/cart',
+        component: Cart
+      }
+    ]
+  }
+]
+
+export {routes}
